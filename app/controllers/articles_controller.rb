2afba5d459dd91e8243948a7737d64e163a13f76
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2024-04-17 15:32:34 +0100

    show articles by id

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Articles.a;;
+    @articles = Articles.all
+  end
+
+  def show
+    @article = Articles.find(params[:id])
   end
 end
