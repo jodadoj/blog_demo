3bc7a78cf7556097d519a299909bf07b5dada579
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2024-04-18 00:27:21 +0100

    added auth - end

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "hi", password: "there", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,22 @@
 class CommentsController < ApplicationController
+
+    http_basic_authenticate_with name: "hi", password: "there", only: [:destroy]
+
     def create
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
         redirect_to article_path(@article)
     end
 
+    def destroy
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+        @comment.destroy
+        redirect_to article_path(@article), status: :see_other
+    end
+
     private
-        def comment_paramsparams.require(:comment).permit(:commenter, :body, :status)
+        def comment_params
+            params.require(:comment).permit(:commenter, :body, :status)
         end
 end
