b69a2fb6eadfd6029b743b839aa524f0b2b0ba6f
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2024-04-17 20:47:18 +0100

    added new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,29 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Articles.all
+    @articles = Article.all
   end
 
   def show
-    @article = Articles.find(params[:id])
+    @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+
 end
